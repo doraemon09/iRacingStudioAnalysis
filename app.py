ff7372aabef060bf0708b28f026dfb149ae0fb16
doraemon09 <1687256+doraemon09@users.noreply.github.com>
2024-11-12 00:16:03 -0800

    Added process_ibt_telemetry_info() + pandas for dataframe

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import werkzeug.utils
 import os
 import yaml
+import pandas as pd
 import irsdk
 
 
@@ -46,23 +47,23 @@ def upload_file():
             try:
                 file.save(filepath)
 
-                # Session Info
+                # Session Info | gets obj
                 session_data = get_session_info(filepath)
 
-                # Telemetry Info
+                # Telemetry Info | gets dataframe
                 telemetry_data = get_telemetry_info(filepath)
 
                 return render_template(
                     'display.html',
                     session_info=session_data,
-                    telemetry_info=telemetry_data,
+                    telemetry_info=telemetry_data.to_dict(orient='records'),
                     yaml_info=yaml_data,
                 )
             except Exception as err:
                 return f"Error processing file: {err}"
         else:
             return "Invalid file extension. iRacing telemetry (.ibt) file only!"
-
+    # Else
     return render_template('index.html', yaml_info=yaml_data)
 
 
@@ -89,11 +90,44 @@ def get_telemetry_info(filepath):
         # Read uploaded file
         ibt_telemetry_info.open(filepath)
 
-        return ibt_telemetry_info
+        # Store data into a dataframe
+        telemetry_info = process_ibt_telemetry_info(ibt_telemetry_info)
+
+        return telemetry_info
     except Exception as err:
         return f"Error reading file: {err}"
 
 
+# Process telemetry data into dataframe
+def process_ibt_telemetry_info(ibt_telemetry_info):
+    try:
+        # Initialize new dictionary
+        telemetry_dict = {}
+
+        # Loop header with get_all() to retrieve data (list)
+        for header in ibt_telemetry_info.var_headers_names:
+            try:
+                # Assigned retrieved data set
+                telemetry_dict[header] = ibt_telemetry_info.get_all(header)
+            except Exception as err:
+                # Set to empty if header is not found in ibt_telemetry_info
+                # Which should not happen to begine with
+                telemetry_dict[header] = []
+                print(f"Error retrieving data for {header}: {err}")
+
+        # Create dataframe with telemetry_dict
+        telemetry_dataframe = pd.DataFrame(telemetry_dict)
+
+        # Replace missing data values (NaN) with 0
+        telemetry_dataframe.fillna(0, inplace=True)
+
+        return telemetry_dataframe
+    except Exception as err:
+        print(f"Error processing telemetry info: {err}")
+        return []
+
+
+# ====================
 if __name__ == "__main__":
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
     app.run(debug=True)

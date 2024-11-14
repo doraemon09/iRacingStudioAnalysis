c03bb0421bae5e5942661f39e18e1e04622b33af
doraemon09 <1687256+doraemon09@users.noreply.github.com>
2024-11-14 07:46:29 -0800

    Disabled default sort on DataTable()

--- static/js/script.js
+++ static/js/script.js
@@ -87,5 +87,6 @@ document.addEventListener('DOMContentLoaded', () => {
     $('table').DataTable({
         lengthMenu: [ 5, 10, 15, 20, 25 ],
         pageLength: 15,
+        "order": [], // Disable default sort onload
     });
 });

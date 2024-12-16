e5e701a6139b09993b52260280e5b52a715fd0ca
doraemon09 <1687256+doraemon09@users.noreply.github.com>
2024-12-15 16:12:44 -0800

    Naming scheme updates

--- static/js/script.js
+++ static/js/script.js
@@ -205,14 +205,13 @@ function formatWithPlus(value) {
     return value > 0 ? `+${value}` : value;
 }
 
-// radian to degree
+// Convert radian to degree
 function radian2Degree(radian) {
     return radian * (180 / Math.PI);
 }
 
-
-// Assign bearing based on degree of angle
-function compassBearing(degree) {
+// Assign direction based on compass bearing
+function bearing2Direction(degree) {
     if(337.5 <= degree || degree < 22.5) {
         return "N"
     } else if(22.5 <= degree && degree < 67.5) {
